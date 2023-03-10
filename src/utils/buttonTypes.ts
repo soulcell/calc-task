b8223ba41f90d244bb5f32bee1a558161aa4f577
soulcell <arsen332211@gmail.com>
2023-03-10 16:16:50 +0300

    feat: add type guards for buttontypes

--- src/utils/buttonTypes.ts
+++ src/utils/buttonTypes.ts
@@ -1,17 +1,39 @@
-export type ButtonDigit =
-  | "0"
-  | "1"
-  | "2"
-  | "3"
-  | "4"
-  | "5"
-  | "6"
-  | "7"
-  | "8"
-  | "9";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const DIGITS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] as const;
+export type ButtonDigit = (typeof DIGITS)[number];
+
+const OPERATORS = ["+", "-", "*", "/"] as const;
+export type ButtonOperator = (typeof OPERATORS)[number];
+
+const ACTIONS = ["=", "C", "CE"] as const;
+export type ButtonAction = (typeof ACTIONS)[number];
+
 export type ButtonPoint = ".";
-export type ButtonOperator = "+" | "-" | "*" | "/";
-export type ButtonAction = "=" | "C" | "CE";
 
 type ButtonType = ButtonDigit | ButtonPoint | ButtonOperator | ButtonAction;
 export default ButtonType;
+
+export function isButtonDigit(x: any): x is ButtonDigit {
+  return DIGITS.includes(x);
+}
+
+export function isButtonPoint(x: any): x is ButtonPoint {
+  return x === ".";
+}
+
+export function isButtonOperator(x: any): x is ButtonOperator {
+  return OPERATORS.includes(x);
+}
+
+export function isButtonAction(x: any): x is ButtonAction {
+  return ACTIONS.includes(x);
+}
+
+export function isButtonType(x: any): x is ButtonType {
+  return (
+    isButtonDigit(x) ||
+    isButtonOperator(x) ||
+    isButtonAction(x) ||
+    isButtonPoint(x)
+  );
+}
