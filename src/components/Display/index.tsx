6a81a3250756d0b95f083a5c966c21a42f6bcadd
soulcell <arsen332211@gmail.com>
2023-03-13 21:22:48 +0300

    feat: add command display

--- src/components/Display/index.tsx
+++ src/components/Display/index.tsx
@@ -1,9 +1,16 @@
 import { useSelector } from "react-redux";
 import selectCalculator from "../../store/reducers/calculator/selector";
-import { StyledDisplay } from "./styled";
+import { OperandCommand, StyledDisplay } from "./styled";
 
 export default function Display(): JSX.Element {
   const calculator = useSelector(selectCalculator);
 
-  return <StyledDisplay>{calculator.operand}</StyledDisplay>;
+  return (
+    <StyledDisplay>
+      <OperandCommand>
+        {calculator.command?.operand} {calculator.command?.symbol}
+      </OperandCommand>
+      {calculator.value}
+    </StyledDisplay>
+  );
 }
--- src/components/Display/styled.ts
+++ src/components/Display/styled.ts
@@ -2,9 +2,14 @@ import styled from "styled-components";
 
 export const StyledDisplay = styled.div`
   grid-area: display;
-  //margin: 0 23px 0 42.5px;
   padding: 20px;
   border-bottom: 1px solid ${(props) => props.theme.border};
   font-size: 2em;
   text-align: right;
+`;
+
+export const OperandCommand = styled.div`
+  margin: -16px 0;
+  font-size: 0.5em;
+  color: ${(props) => props.theme.border};
 `;
