75cfb0acba4ee14888d055a3ad4cb8c6dd8753c9
soulcell <arsen332211@gmail.com>
2023-03-15 21:54:53 +0300

    feat: add error boundary

--- src/index.tsx
+++ src/index.tsx
@@ -7,19 +7,22 @@ import reportWebVitals from "./reportWebVitals";
 import { persistor, store } from "./store/store";
 import StyledApp from "./styled";
 import { PersistGate } from "redux-persist/integration/react";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <StyledApp>
-          <RouterProvider router={router} />
-        </StyledApp>
-      </PersistGate>
-    </Provider>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <StyledApp>
+            <RouterProvider router={router} />
+          </StyledApp>
+        </PersistGate>
+      </Provider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
 
