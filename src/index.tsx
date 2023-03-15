47908d820f5d45712281e8ba5bbeda2e7b941dd1
soulcell <arsen332211@gmail.com>
2023-03-15 21:46:47 +0300

    feat: add redux persist

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,9 @@ import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import reportWebVitals from "./reportWebVitals";
-import { store } from "./store/store";
+import { persistor, store } from "./store/store";
 import StyledApp from "./styled";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -13,9 +14,11 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <StyledApp>
-        <RouterProvider router={router} />
-      </StyledApp>
+      <PersistGate loading={null} persistor={persistor}>
+        <StyledApp>
+          <RouterProvider router={router} />
+        </StyledApp>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,16 +2,37 @@ import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 import { historyMiddleware } from "./middlewares/historyMiddleware";
 import { intermediateValueMiddleware } from "./middlewares/intermediateValueMiddleware";
-import rootReducer from "./reducers/rootReducer";
+import rootReducer, { AppState } from "./reducers/rootReducer";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PersistConfig,
+  persistReducer,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistStore,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig: PersistConfig<AppState> = {
+  key: "root",
+  storage,
+  blacklist: ["calculator"],
+};
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistReducer(persistConfig, rootReducer),
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat([
-      intermediateValueMiddleware,
-      historyMiddleware,
-    ]),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat([intermediateValueMiddleware, historyMiddleware]),
 });
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch: () => AppDispatch = useDispatch;
