ba8a47f1fb78f100b5fb62f8bb553abc1f65ab08
soulcell <arsen332211@gmail.com>
2023-04-06 23:11:42 +0300

    feat: (review 22) add error message to error boundary fallback

--- src/components/ErrorBoundary/index.tsx
+++ src/components/ErrorBoundary/index.tsx
@@ -5,17 +5,17 @@ interface Props {
 }
 
 interface State {
-  hasError: boolean;
+  error?: Error;
 }
 
 class ErrorBoundary extends Component<Props, State> {
   public state: State = {
-    hasError: false,
+    error: undefined,
   };
 
-  public static getDerivedStateFromError(): State {
+  public static getDerivedStateFromError(error: Error): State {
     // Update state so the next render will show the fallback UI.
-    return { hasError: true };
+    return { error };
   }
 
   public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
@@ -23,11 +23,16 @@ class ErrorBoundary extends Component<Props, State> {
   }
 
   public render() {
-    const { hasError } = this.state;
+    const { error } = this.state;
     const { children } = this.props;
 
-    if (hasError) {
-      return <h1>Sorry.. there was an error</h1>;
+    if (error) {
+      return (
+        <>
+          <h1>Sorry.. there was an error</h1>
+          {error.message}
+        </>
+      );
     }
 
     return children;
