0fbfcadfdec62205c7ea92689a757e18dfd93efc
soulcell <arsen332211@gmail.com>
2023-04-07 17:08:35 +0300

    fix: fix number validation regex

--- src/utils/tokenValidation.ts
+++ src/utils/tokenValidation.ts
@@ -2,7 +2,7 @@ import { ACCURACY } from "@constants/calculator";
 
 export function isNumericToken(token: string) {
   const regex = new RegExp(
-    `^-?([1-9]\\d*(\\.)\\d{0,${ACCURACY}}|0?(\\.)\\d{0,${ACCURACY}}|[1-9]\\d{0,${ACCURACY}}|0)$`
+    `^-?([1-9]\\d*(\\.)\\d{0,${ACCURACY}}|0?(\\.)\\d{0,${ACCURACY}}|[1-9]\\d*|0)$`
   );
   return regex.test(token);
 }
