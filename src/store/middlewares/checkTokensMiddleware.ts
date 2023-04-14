3af38038044f4a7b715d106165bc53cdee3ab303
soulcell <arsen332211@gmail.com>
2023-04-14 16:26:30 +0300

    refactor: remove comments

--- src/store/middlewares/checkTokensMiddleware.ts
+++ src/store/middlewares/checkTokensMiddleware.ts
@@ -58,34 +58,6 @@ const checkTokensMiddleware: Middleware<object, AppState> =
     }
 
     return next(action);
-
-    // if (
-    //   !executeCommand.match(action) &&
-    //   !appendOperatorToken.match(action)
-    //   ) {
-    //   return next(action);
-    // }
-
-    // const { tokens } = api.getState().calculator;
-
-    // if (tokens.length === 0) {
-    //   return;
-    // }
-    // const lastToken = tokens[tokens.length - 1];
-
-    // if (isOperatorToken(lastToken)) {
-    //   return;
-    // }
-
-    // if (
-    //   lastToken &&
-    //   isNumericToken(lastToken) &&
-    //   !Number.isFinite(+lastToken)
-    // ) {
-    //   api.dispatch(clearValue());
-    // }
-
-    // return next(action);
   };
 
 export default checkTokensMiddleware;
