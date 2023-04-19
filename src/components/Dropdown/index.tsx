a3e83a9159e85341dc9bb2a7b84ee216c04575f0
soulcell <arsen332211@gmail.com>
2023-04-19 10:35:36 +0300

    "revert: moved dropdown logic into custom hook"
    This reverts commit 9773b2a2c830ee17ddf5c3b39daca6f12727a221.

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,5 +1,17 @@
-import SVG from "@/components/SVG";
+import {
+  Children,
+  cloneElement,
+  isValidElement,
+  ReactElement,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
 
+import SVG from "@/components/SVG";
+import useClickOutside from "@/hooks/useClickOutside";
+
+import { DropdownItemProps } from "./DropdownItem/types";
 import {
   DropdownButton,
   DropdownList,
@@ -7,15 +19,47 @@ import {
   StyledDropdown,
 } from "./styled";
 import { DropdownProps } from "./types";
-import useDropdown from "./useDropdown";
 
 export default function Dropdown({
   children,
   onSelectedValueChanged,
   dataCy,
 }: DropdownProps): JSX.Element {
-  const { dropdownItems, selectedTitle, isOpen, toggleOpen, triggerRef } =
-    useDropdown(children, onSelectedValueChanged);
+  const [isOpen, setOpen] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [selectedValue, setSelectedValue] = useState("");
+  const [selectedTitle, setSelectedTitle] = useState("");
+
+  const handleClickOutside = useCallback(() => setOpen(false), []);
+  const triggerRef = useClickOutside<HTMLDivElement>(handleClickOutside);
+
+  useEffect(() => {
+    const el = Children.toArray(children)[selectedIndex];
+    if (isValidElement<DropdownItemProps>(el)) {
+      setSelectedValue(el.props.value);
+      setSelectedTitle(el.props.title);
+    }
+  }, [selectedIndex, children]);
+
+  useEffect(() => {
+    if (onSelectedValueChanged) {
+      onSelectedValueChanged(selectedValue);
+    }
+  }, [selectedValue, onSelectedValueChanged]);
+
+  useEffect(() => {
+    let idx = 0;
+    Children.forEach(children, (child, index) => {
+      if (isValidElement<DropdownItemProps>(child)) {
+        if (child.props.default) {
+          idx = index;
+        }
+      }
+    });
+    setSelectedIndex(idx);
+  }, []);
+
+  const toggleOpen = useCallback(() => setOpen(!isOpen), [isOpen]);
 
   return (
     <StyledDropdown ref={triggerRef} data-cy={dataCy}>
@@ -23,7 +67,19 @@ export default function Dropdown({
         <SelectedTitle>{selectedTitle}</SelectedTitle>
         <SVG icon="arrow" width="24px" height="24px" />
       </DropdownButton>
-      <DropdownList isOpen={isOpen}>{dropdownItems}</DropdownList>
+      <DropdownList isOpen={isOpen}>
+        {Children.map(children, (child, idx) => {
+          if (isValidElement(child)) {
+            return cloneElement(child as ReactElement<DropdownItemProps>, {
+              isSelected: idx === selectedIndex,
+              onClick: () => {
+                setSelectedIndex(idx);
+                setOpen(false);
+              },
+            });
+          }
+        })}
+      </DropdownList>
     </StyledDropdown>
   );
 }
